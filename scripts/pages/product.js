0e2fad2af507f64f41dd6f41839b7ef93a2da575
qiang zeng <qzeng2490@yahoo.com>
2015-11-09 10:31:52 -0800

    add style to three pages

--- scripts/pages/product.js
+++ scripts/pages/product.js
@@ -1,4 +1,21 @@
 //
 module.exports = function() {
- 
+  
+  $("#SideNewProducts").remove()
+  $("#SideNewsletterBox").remove()
+  $("#ProductBreadcrumb").remove()
+  $("#ProductByCategory").remove()
+  $("#ProductReviews > a").remove()
+
+
+  $("#ImageScrollPrev").remove()
+  $("#ImageScrollNext").remove()
+
+  $("#SideProductRelated").remove()
+  $("#SideProductRecentlyViewed").remove()
+
+
+
+  $("#SimilarProductsByCustomerViews").remove()
+  
 };
